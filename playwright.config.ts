1e292dd2183a29799f486a2e06f69bd17a1271c9
NewLara <NewLara@users.noreply.github.com>
2026-04-04 11:17:17 -0500

    added output dir

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  outputDir: './test-results',
   use: {
     baseURL: 'https://opensource-demo.orangehrmlive.com',
     browserName: 'chromium',
     headless: false,
     screenshot: 'only-on-failure',
+    video: 'off',
   },
 });
